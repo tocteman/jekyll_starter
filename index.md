03b3e49a864284bce8e0a34e73235344d1f76436
tocteman <juanfrancisco.vinueza@gmail.com>
2021-01-30 00:54:28 -0500

    + stimulus!

--- index.md
+++ index.md
@@ -2,5 +2,10 @@
 # Feel free to add content and custom Front Matter to this file.
 # To modify the layout, see https://jekyllrb.com/docs/themes/#overriding-theme-defaults
 
-layout: home
+layout: default
 ---
+<div data-controller="hello">
+  <input data-target="hello.name" type="text">
+  <button data-action="click->hello#greet">Greet</button>
+</div>
+
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const logMessage = "ES6 & Stimulus with Jekyll on Webpack";
+console.log(logMessage)
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
